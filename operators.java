c285970b192563c617c04bf394a2213e9672884f
jonathan <jonathan@localhost.localdomain>
2020-01-20 17:10:52 +0300

    multiplication

---
+++ operators.java
@@ -0,0 +1,8 @@
+public class operators{
+    public static void main(String[] args) {
+        int x= 10;
+        int y =20;
+        System.out.println("x minus y is " + (x-y));
+        System.out.println("x times y is " + (x*y));
+    }
+}
