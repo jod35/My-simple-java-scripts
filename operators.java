4597536bc55d72dbccbf1c9141b5e05eca4641dd
jonathan <jonathan@localhost.localdomain>
2020-01-20 17:25:44 +0300

    addition

--- operators.java
+++ operators.java
@@ -4,6 +4,7 @@ public class operators{
         int y =20;
         System.out.println("x minus y is " + (x-y));
         System.out.println("x times y is " + (x*y));
-        System.out.println("x divided with y is" + (x/y)); // 0 because they are integers
+        System.out.println("x divided with y is " + (x/y)); // 0 because they are integers
+        System.out.println("x added to y is " +(x+y));
     }
 }
